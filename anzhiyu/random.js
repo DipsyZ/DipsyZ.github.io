8ce77ff6c8718fe20567f403a3f6bc81ae16e54b
dcdd <zyt08097@163.com>
2023-12-26 22:41:26 +0800

    Site updated: 2023-12-26 22:41:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/22/hello-world/","2023/12/23/论文总结1/"];function toRandomPost(){
+var posts=["2023/12/23/论文总结1/","2023/12/22/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
